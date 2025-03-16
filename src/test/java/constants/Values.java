e468a124cb1115653045ae1d061d3a39497a3f9b
aalmoskovskiy <aalmoskovskiy@sberbank.ru>
2025-03-16 19:50:24 +0300

    Добавление 3 тестов по проверке погоды

---
+++ src/test/java/constants/Values.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class Values {
+    /**
+     * Погода в Сан-Франциско от 16.03.2025
+     */
+    public static final int EXPECTED_WEATHER_IN_SAN_FRANTZ = 12;
+    public static final int WEATHER_IN_SAN_FRANTZ = 12;
+
+    /**
+     * Погода в Даллас от 16.03.2025
+     */
+    public static final int EXPECTED_WEATHER_IN_DALLAS = 18;
+    public static final int WEATHER_IN_DALLAS = 20;
+}
---
+++ src/test/java/tests/CheckWeatherInSanFrantzTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static constants.Values.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CheckWeatherInSanFrantzTest {
+
+    @Test
+    @DisplayName("Погода в Сан-Франциско должна быть 12 градусов")
+    public void testWeatherInSanFrantz() {
+        assertEquals(EXPECTED_WEATHER_IN_SAN_FRANTZ, WEATHER_IN_SAN_FRANTZ, "Ожидается, что погода в Сан-Франциско будет " + EXPECTED_WEATHER_IN_SAN_FRANTZ + " градусов");
+    }
+
+    @Test
+    @DisplayName("Погода в Сан-Франциско должна быть 18 градусов")
+    public void testWeatherInDallas() {
+        assertEquals(EXPECTED_WEATHER_IN_DALLAS, WEATHER_IN_DALLAS, "Ожидается, что погода в Далласе будет " + EXPECTED_WEATHER_IN_DALLAS + " градусов");
+    }
+
+    @Test
+    @DisplayName("Сравнение погоды в Сан-Франциско и Далласе")
+    public void testWeatherInSanFrantzMoreDallas() {
+        assertTrue(WEATHER_IN_SAN_FRANTZ < WEATHER_IN_DALLAS, "Ожидается, что погода в Сан-Франциско (" + WEATHER_IN_SAN_FRANTZ +
+                ") будет меньше, чем в Далласе (" + WEATHER_IN_DALLAS + ")");
+    }
+}
